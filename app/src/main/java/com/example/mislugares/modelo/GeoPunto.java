91e55d358e18405dd20c5da8e98e361364a6f447
CarlosMape <cagrmape@gmail.com>
2020-02-12 20:35:41 +0100

    Editing GeoPunto POJO and creating Ususario POJO

--- app/src/main/java/com/example/mislugares/modelo/GeoPunto.java
+++ app/src/main/java/com/example/mislugares/modelo/GeoPunto.java
@@ -6,25 +6,31 @@ public class GeoPunto {
 
     private double longitud, latitud;
 
-    static public GeoPunto SIN_POSICION = new GeoPunto(0.0,0.0);
+    static public GeoPunto SIN_POSICION = new GeoPunto(0.0, 0.0);
+
+    public GeoPunto() {
+
+    }
 
     public GeoPunto(double longitud, double latitud) {
-        this.longitud= longitud;
-        this.latitud= latitud;
+        this.longitud = longitud;
+        this.latitud = latitud;
     }
+
     public String toString() {
-        return new String("longitud:" + longitud + ", latitud:"+ latitud);
+        return new String("longitud:" + longitud + ", latitud:" + latitud);
     }
+
     public double distancia(GeoPunto punto) {
         final double RADIO_TIERRA = 6371000; // en metros
         double dLat = Math.toRadians(latitud - punto.latitud);
         double dLon = Math.toRadians(longitud - punto.longitud);
         double lat1 = Math.toRadians(punto.latitud);
         double lat2 = Math.toRadians(latitud);
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.sin(dLon/2) * Math.sin(dLon/2) *
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.sin(dLon / 2) * Math.sin(dLon / 2) *
                         Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return c * RADIO_TIERRA;
     }
 
